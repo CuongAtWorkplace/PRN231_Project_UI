90cb26533bd6ba1de3b0acc52f3881c7cb2aa65f
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-07 16:37:36 +0700

    update

--- projectprn231/src/App.js
+++ projectprn231/src/App.js
@@ -6,15 +6,48 @@ import { ListGenre } from './compoments/Leader/ListGenre';
 import { ListAssignTask } from './compoments/Leader/ListAssignTask';
 import { Writer } from './compoments/Writer/Writer';
 import { ViewTaskDetail } from './compoments/Leader/ViewTaskDetail';
-//import { Reporter } from './compoments/Reporter/Reporter';
 import { ImportFile } from './compoments/TestFile/ImportFile';
+//import { NavBar } from './compoments/NavBar/NavBar';
+//import {SideBar} from '/compoments/SideBar/SideBar'
+import { ListReportTask } from './compoments/Reporter/ListReportTask';
+import { ListReject } from './compoments/Leader/ListReject';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {Table} from './compoments/table/Table'
 import { CommentBrowseTable } from './compoments/table/CommentBrowseTable';
 function App() {
   return (
-    <CommentBrowseTable/>
+
+    //<Leader/>
+    //<UpdateAssignTask/>
+    //<Reporter/>
+    <>
+      {/* <ListGenre /> */}
+      {/* <Leader/> */}
+      {/* <UpdateAssignTask/> */}
+      {/* <ListAssignTask/> */}
+      {/* <ViewTaskDetail/> */}
+      {/* <Writer/> */}
+      {/* <Reporter/> */}
+      {/* <ImportFile/> */}
+      {/* <SideBar/> */}
+      {/* <ListReportTask/> */}
+      <ListReject/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+
+
   );
 }
 
