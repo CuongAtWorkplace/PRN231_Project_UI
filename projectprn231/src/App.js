a537e6db542c1ba4e61b00b008254b3271f82111
Tuan Anh <tuananh462001@gmail.com>
2023-06-10 18:43:18 +0700

    Submit Change

--- projectprn231/src/App.js
+++ projectprn231/src/App.js
@@ -8,14 +8,17 @@ import { Writer } from './compoments/Writer/Writer';
 import { ViewTaskDetail } from './compoments/Leader/ViewTaskDetail';
 import { ImportFile } from './compoments/TestFile/ImportFile';
 import { NavBar } from './compoments/NavBar/NavBar';
-import {SideBar} from './compoments/SideBar/SideBar';
+import { SideBar } from './compoments/SideBar/SideBar';
 import { ListReportTask } from './compoments/Reporter/ListReportTask';
 import { ListReject } from './compoments/Leader/ListReject';
 import { ListWritingTask } from './compoments/Writer/ListWritingTask';
+import { RedirectPage } from './compoments/TestFile/RedirectPage';
 import { ToDoReportTask } from './compoments/Reporter/ToDoReportTask';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Table} from './compoments/table/Table'
+import { BrowserRouter, Switch, Route, Routes, NavLink } from 'react-router-dom';
+import { Navigate} from 'react-router-dom';
+import { Table } from '../src/Pages/Admin'
 import { CommentBrowseTable } from './compoments/table/CommentBrowseTable';
 import { Admin_Comment } from './Pages/Admin_Comment';
 function App() {
@@ -26,19 +29,29 @@ function App() {
     //<UpdateAssignTask/>
     //<Reporter/>
     <>
-      {/* <ListGenre /> */}
-      {/* <Leader/> */}
-      {/* <UpdateAssignTask/> */}
-      {/* <ListAssignTask/> */}
-      {/* <ViewTaskDetail/> */}
-      {/* <Writer/> */}
-      {/* <Reporter/> */}
-      {/* <ImportFile/> */}
-      {/* <SideBar/> */}
-      {/* <ListReportTask/> */}
-      {/* <ListReject/> */}
-      {/* <ListWritingTask/> */}
-      <ToDoReportTask/>
+      <BrowserRouter>
+        {/* <ListGenre /> */}
+        {/* <Leader/> */}
+        {/* <UpdateAssignTask/> */}
+        {/* <ListAssignTask/> */}
+        {/* <ViewTaskDetail/> */}
+        {/* <Writer/> */}
+        {/* <Reporter/> */}
+        {/* <ImportFile/> */}
+        {/* <SideBar/> */}
+        {/* <ListReportTask/> */}
+        {/* <ListReject/> */}
+        {/* <ListWritingTask/> */}
+        {/* <ToDoReportTask/> */}
+        {/* <RedirectPage /> */}
+        <NavLink to="/home">
+          <button>Well</button>
+        </NavLink>
+        <Routes>
+          <Route path='/home' element={<RedirectPage />} />
+          <Route path='/reporter' element={<Reporter />} />
+        </Routes>
+      </BrowserRouter>
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -51,6 +64,7 @@ function App() {
         pauseOnHover
         theme="colored"
       />
+
     </>
 
 
