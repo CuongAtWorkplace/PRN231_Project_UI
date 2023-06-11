bb7f2fda2d2c4b3fda5e87093a0081df2a9663c1
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-11 17:44:46 +0700

    update

--- projectprn231/src/App.js
+++ projectprn231/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Leader } from './compoments/Leader/Leader';
 import { UpdateAssignTask } from './compoments/Leader/UpdateAssigTask';
-//import { Reporter } from './compoments/Reporter/Reporter';
 import { ListGenre } from './compoments/Leader/ListGenre';
 import { ListAssignTask } from './compoments/Leader/ListAssignTask';
 import { Writer } from './compoments/Writer/Writer';
@@ -18,7 +17,6 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Switch, Route, Routes, NavLink } from 'react-router-dom';
 import { Navigate} from 'react-router-dom';
-import { Table } from '../src/Pages/Admin'
 import { CommentBrowseTable } from './compoments/table/CommentBrowseTable';
 import { Admin_Comment } from './Pages/Admin_Comment';
 import { Chart } from './compoments/chart/Chart';
@@ -26,52 +24,7 @@ import { Admin_Home } from './Pages/Admin_Home';
 import { Admin_UserManagement } from './Pages/Admin_UserManagement';
 function App() {
   return (
-
-
-
-    //<Leader/>
-    //<UpdateAssignTask/>
-    //<Reporter/>
-    <>
-      <BrowserRouter>
-        {/* <ListGenre /> */}
-        {/* <Leader/> */}
-        {/* <UpdateAssignTask/> */}
-        {/* <ListAssignTask/> */}
-        {/* <ViewTaskDetail/> */}
-        {/* <Writer/> */}
-        {/* <Reporter/> */}
-        {/* <ImportFile/> */}
-        {/* <SideBar/> */}
-        {/* <ListReportTask/> */}
-        {/* <ListReject/> */}
-        {/* <ListWritingTask/> */}
-        {/* <ToDoReportTask/> */}
-        {/* <RedirectPage /> */}
-        <NavLink to="/home">
-          <button>Well</button>
-        </NavLink>
-        <Routes>
-          <Route path='/home' element={<RedirectPage />} />
-          <Route path='/reporter' element={<Reporter />} />
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-
-    </>
-
-
+   <Admin_Home/>
   );
 }
 
