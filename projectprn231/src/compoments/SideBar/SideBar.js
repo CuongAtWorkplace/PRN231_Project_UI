3e566f3cef9577378b14671a992a855d91335787
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-11 18:37:35 +0700

    update

--- projectprn231/src/compoments/SideBar/SideBar.js
+++ projectprn231/src/compoments/SideBar/SideBar.js
@@ -4,12 +4,20 @@ import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import LogoutIcon from '@mui/icons-material/Logout';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import OpenWithIcon from '@mui/icons-material/OpenWith';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import React, { Component } from "react";
 
 export class SideBar extends Component {
 
     render() {
         return (
+            <div className="layout">
+
             <div className="sidebar">
                 <div className="top">
                     <span className="logo">
@@ -54,6 +62,48 @@ export class SideBar extends Component {
 
                 </div>
 
+            </div>
+
+            <div className="layout_sidebar">
+            <div className="navbar">
+                <div className="wrapper">
+                    <div className="search">
+                        <input type="text" placeholder="Search  ... " />
+                        <SearchIcon />
+                    </div>
+
+                    <div className="items">
+                        <div className="items">
+                            <LanguageIcon className="icon" />
+                            English
+                        </div>
+
+                        <div className="item">
+                            <DarkModeIcon className="icon" />
+                        </div>
+
+                        <div className="item">
+                            <OpenWithIcon className="icon" />
+                        </div>
+                        <div className="item">
+                            <NotificationsNoneIcon className="icon" />
+                            <div className="counter">1</div>
+                        </div>
+                        <div className="item">
+                            <FormatListBulletedIcon className="icon" />
+                        </div>
+
+                        <div className="item">
+                        </div>
+                        <img src="https://th.bing.com/th/id/R.e900afd2d9b0b93857b0ffa8310f5247?rik=e6eWtgl4bA%2bbGg&pid=ImgRaw&r=0"
+                            alt=""
+                            className="userAvantar"
+                        />
+                    </div>
+
+                </div>
+            </div>
+            </div>
             </div>
         )
     }
