45fe39fbc3f60d6cd980cdb56bea5bf02472000f
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-11 18:39:54 +0700

    alo

--- projectprn231/src/App.js
+++ projectprn231/src/App.js
@@ -24,7 +24,7 @@ import { Admin_Home } from './Pages/Admin_Home';
 import { Admin_UserManagement } from './Pages/Admin_UserManagement';
 function App() {
   return (
-   <SideBar/>
+   <NavBar/>
   );
 }
 
