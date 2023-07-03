563bf868c82669d68f431903963b1084ff4fab41
nhatvm10 <nhatvuminh00@gmail.com>
2023-07-03 21:39:51 +0700

    commit

--- projectprn231/src/compoments/Home/UserDetail.js
+++ projectprn231/src/compoments/Home/UserDetail.js
@@ -1,7 +1,46 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import jwtDecode from "jwt-decode";
 export class UserDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userdetail : {},
+            userid:null,
+        }
+    }
+   
+    refreshUser() {
+        const{userid} = this.state;
+        fetch(`https://localhost:7248/api/User/GetUserById?id=${userid}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ userdetail : data });
+            });
+    }
+
+    componentDidMount() {
+        const token = localStorage.getItem("token");
+
+   
+    if (token != null) {
+
+        const decodedToken = jwtDecode(token);
+
+        const userid = decodedToken.id;
+        this.setState({ userid }, () => {
+            this.refreshUser();
+          });
+     
+        // this.setState({nameUser : decodedToken.FullName});
+    }
+        
+    }
+
     render() {
+        const{userdetail} = this.state;
+       
         return (
             <div>
                 <Header />
@@ -11,19 +50,16 @@ export class UserDetail extends Component {
                             <div class="col-md-4 border-right">
                                 <div class="d-flex flex-column align-items-center text-center">
                                      <ul class="p-3 py-5">
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-                                    <li><a>aaaaaaaaaaaaa</a></li>
-
+                                    <li><a>Thông tin tài khoản</a></li>
+                                    <li><a>Nâng cấp</a></li>
+                                    <li><a>Chat</a></li>
+                                 
                                 </ul> 
                                 </div>
                               
                             </div>
                             <div class="col-md-2 border-right">
-                                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+                                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span class="font-weight-bold">{userdetail.fullName}</span><span> </span></div>
                             </div>
                             <div class="col-md-4 border-right">
                                 <div class="p-3 py-5">
@@ -31,19 +67,17 @@ export class UserDetail extends Component {
                                         <h4 class="text-right">Profile Settings</h4>
                                     </div>
                                     <div class="row mt-2">
-                                        <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value="" /></div>
-                                        <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname" /></div>
+                                        <div class="col-md-12"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value={userdetail.fullName} /></div>
                                     </div>
                                     <div class="row mt-3">
-                                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value="" /></div>
-                                        <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value="" /></div>
-                                        <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                                        <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
+                                    <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control" placeholder="enter email" value={userdetail.email} /></div>
+                                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value={userdetail.phone} /></div>
+                                        <div class="col-md-12"><label class="labels">Address </label><input type="text" class="form-control" placeholder="enter address " value={userdetail.address} /></div>
                                     </div>
-                                    <div class="row mt-3">
+                                    {/* <div class="row mt-3">
                                         <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value="" /></div>
                                         <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state" /></div>
-                                    </div>
+                                    </div> */}
                                     <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
                                 </div>
                             </div>
@@ -57,3 +91,4 @@ export class UserDetail extends Component {
     }
 
 }
+export default withRouter(UserDetail);
